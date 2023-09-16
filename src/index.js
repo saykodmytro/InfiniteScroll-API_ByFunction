bcd6319dcd1b77c8a17aea4382606103aac1ecb9
Dmytro Sayko <saykodmytro@icloud.com>
2023-09-16 10:07:37 +0300

    up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,75 @@
-console.log('Hello Dima');
+import { PixabayAPI } from './js/pixabay-api';
+import { createGalleryCard } from './js/createGalleryCard';
+import { galleryEl, loadMoreBtn, loaderEl, formEl } from './js/refs';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import {
+  onError,
+  onEmpty,
+  addLoader,
+  hideLoader,
+  showMoreBtn,
+  hideMoreBtn,
+  messageTotalPhoto,
+} from './js/function.js';
+
+const pixabayApi = new PixabayAPI(40);
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+formEl.addEventListener('submit', onSubmit);
+loadMoreBtn.addEventListener('click', onMoreData);
+
+async function onSubmit(evt) {
+  evt.preventDefault();
+  pixabayApi.page = 1;
+
+  const searchQuery =
+    evt.currentTarget.elements['user-search-query'].value.trim();
+  pixabayApi.q = searchQuery;
+  if (pixabayApi.q === '') {
+    galleryEl.innerHTML = '';
+    return onEmpty();
+  }
+
+  try {
+    const resp = await pixabayApi.getPhotos();
+    galleryEl.innerHTML = createGalleryCard(resp.hits);
+    lightbox.refresh();
+
+    if (resp.totalHits === 0) {
+      hideMoreBtn();
+      return onError();
+    }
+
+    addLoader();
+
+    messageTotalPhoto(resp.totalHits);
+    resp.total > pixabayApi.perPage ? showMoreBtn() : hideMoreBtn();
+
+    hideLoader();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function onMoreData(evt) {
+  pixabayApi.page += 1;
+
+  try {
+    const resp = await pixabayApi.getPhotos();
+    galleryEl.insertAdjacentHTML('beforeend', createGalleryCard(resp.hits));
+    lightbox.refresh();
+    const averagePage = Math.ceil(resp.total / pixabayApi.perPage);
+    if (averagePage === pixabayApi.page) {
+      hideMoreBtn();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ src/js/createGalleryCard.js
@@ -0,0 +1,24 @@
+export function createGalleryCard(gallery) {
+  return gallery
+    .map(image => {
+      return `<div class="photo-card">
+     <a class = "gallary__item" href="${image.largeImageURL}">
+      <img class ="gallery__image"  src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/></a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: </b>${image.likes}
+      </p>
+      <p class="info-item">
+        <b>Views: </b>${image.views}
+      </p>
+      <p class="info-item">
+        <b>Comments: </b>${image.comments}
+      </p>
+      <p class="info-item">
+        <b>Dowloads: </b>${image.downloads}
+      </p>
+    </div>
+  </div>`;
+    })
+    .join('');
+}
