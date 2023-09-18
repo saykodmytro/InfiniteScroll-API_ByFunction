1302e132ccca0e9e292de1340c742629e03420fd
Dmytro Sayko <saykodmytro@icloud.com>
2023-09-18 20:11:27 +0300

    up

--- src/js/app.js
+++ src/js/app.js
@@ -14,15 +14,75 @@ import {
   messageLastPage,
 } from './function.js';
 
+formEl.addEventListener('submit', onSubmit);
+let currentPage = 1;
+let query = '';
 // ****************************************************************************
-// Виклик функції і очікування результату
-getPhotosFun()
-  .then(result => {
-    // Результат доступний тут, можна його використовувати
-    console.log('Результат:', result);
-  })
-  .catch(err => {
-    // Обробка помилок, якщо вони сталися під час виконання запиту або функції
-    console.error('Помилка виконання запиту або функції:', err);
+
+let options = {
+  root: null,
+  rootMargin: '300px',
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver(onLoad, options);
+
+function onLoad(entries, observe) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      currentPage += 1;
+      getPhotosFun(query, currentPage)
+        .then(result => {
+          console.log('query: ', query);
+          result.page += 1;
+          galleryEl.insertAdjacentHTML(
+            'beforeend',
+            createGalleryCard(result.hits)
+          );
+          console.log('insertAdjacentHTML');
+          console.log('result.totalHits ', result.totalHits);
+          const lastPage = Math.ceil(result.totalHits / 40);
+          console.log('lastPage:', lastPage);
+          if (lastPage === currentPage) {
+            console.log('message');
+            messageLastPage();
+            observer.unobserve(target);
+          }
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
   });
+}
+// ****************************************************************************
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  currentPage = 1;
+  const query = evt.currentTarget.elements['user-search-query'].value.trim();
+  if (query === '') {
+    console.log('pusto');
+    galleryEl.innerHTML = '';
+    return onEmpty();
+  }
+  addLoader();
+  getPhotosFun(query, currentPage)
+    .then(result => {
+      // Результат доступний тут, можна його використовувати
+      galleryEl.innerHTML = createGalleryCard(result.hits);
+      if (result.totalHits === 0) {
+        return onError();
+      }
+      observer.observe(target);
+    })
+    .catch(err => {
+      // Обробка помилок, якщо вони сталися під час виконання запиту або функції
+      console.error('Помилка виконання запиту або функції:', err);
+    })
+    .finally(() => {
+      console.log('finally');
+      hideLoader();
+    });
+}
 // ****************************************************************************
--- src/js/pixabay-api-func.js
+++ src/js/pixabay-api-func.js
@@ -1,25 +1,35 @@
 import axios from 'axios';
-const API_KEY = '39430730-0a1aacc0e107061ec7cb5615a';
-const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=`;
-const FILTER_RESPONSE =
-  '&orientation=horizontal&image_type=photo&safesearch=true&';
+import { onSubmit } from './app';
 
-export async function getPhotosFun() {
+const BASE_URL = 'https://pixabay.com/api/?';
+const API_KEY = '39430730-0a1aacc0e107061ec7cb5615a';
+
+export async function getPhotosFun(query) {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    orientation: 'horizontal',
+    image_type: 'photo',
+    safesearch: true,
+    page: 1,
+    per_page: 40,
+  });
   try {
     // Виконуємо асинхронний запит GET до вказаного URL
-    const response = await axios.get(`${BASE_URL}${FILTER_RESPONSE}`);
+    const endpoint = BASE_URL + params.toString();
+    const response = await axios.get(endpoint);
 
     // Отримуємо дані з відповіді
     const data = response.data;
 
     // Робимо що-небудь із отриманими даними
-    console.log(data);
+    console.log('In pixabay-fun: ', data);
 
     // Повертаємо дані або робимо щось інше з ними
     return data;
   } catch (error) {
     // Обробка помилок, якщо запит не вдалося виконати
-    console.error('Помилка запиту:', error);
+    console.error('Помилка запиту: ', error);
     throw error; // Можна обробляти помилки тут або передавати їх далі
   }
 }
